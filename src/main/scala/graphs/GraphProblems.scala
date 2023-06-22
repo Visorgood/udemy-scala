e45205b59a73b28994deda5698db0000308d2592
Viacheslav Inozemtsev <viacheslav.inozemtsev@gmail.com>
2023-06-22 10:29:55 +0200

    added findPath to graphs

--- src/main/scala/graphs/GraphProblems.scala
+++ src/main/scala/graphs/GraphProblems.scala
@@ -16,13 +16,28 @@ object GraphProblems extends App {
 
   def isPath[T](graph: Graph[T], start: T, end: T): Boolean = {
     @tailrec
-    def findPath(frontier: List[T], visited: Set[T]): Boolean = {
+    def isPathTailrec(frontier: List[T], visited: Set[T]): Boolean = {
       if (frontier.isEmpty) false
-      else if (visited.contains(frontier.head)) findPath(frontier.tail, visited)
+      else if (visited.contains(frontier.head)) isPathTailrec(frontier.tail, visited)
       else if (frontier.head == end) true
-      else findPath(frontier.tail ++ graph(frontier.head), visited + frontier.head)
+      else isPathTailrec(frontier.tail ++ graph(frontier.head), visited + frontier.head)
     }
-    graph.contains(start) && graph.contains(end) && findPath(List(start), Set.empty)
+    graph.contains(start) && graph.contains(end) && isPathTailrec(List(start), Set.empty)
+  }
+
+  def findPath[T](graph: Graph[T], start: T, end: T): List[T] = {
+    @tailrec
+    def findPathTailrec(frontier: List[T], visited: Set[T], acc: List[T]): List[T] = {
+      if (frontier.isEmpty) List.empty
+      else if (frontier.head == end) (end :: acc).reverse
+      else if (visited.contains(frontier.head)) {
+        if (acc.head == frontier.head) findPathTailrec(frontier.tail, visited, acc.tail)
+        else findPathTailrec(frontier.tail, visited, acc)
+      }
+      else findPathTailrec(graph(frontier.head).toList ++ frontier, visited + frontier.head, frontier.head :: acc)
+    }
+    if (!graph.contains(start) || !graph.contains(end)) List.empty
+    else findPathTailrec(List(start), Set.empty, List.empty)
   }
 
   val socialNetwork: Graph[String] = Map(
@@ -42,4 +57,12 @@ object GraphProblems extends App {
   println(isPath(socialNetwork, "Charlie", "Bob"))
   println(isPath(socialNetwork, "Charlie", "Alice"))
   println(isPath(socialNetwork, "Bob", "David"))
+
+  println("findPath")
+  println(findPath(socialNetwork, "Alice", "Mary"))
+  println(findPath(socialNetwork, "Charlie", "Bob"))
+  println(findPath(socialNetwork, "Charlie", "Alice"))
+  println(findPath(socialNetwork, "Bob", "David"))
+  println(findPath(socialNetwork, "Alice", "Tom"))
+  println(findPath(socialNetwork, "Tom", "Alice"))
 }
